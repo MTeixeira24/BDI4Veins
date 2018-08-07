8fb6d493f1eaf127edbe832114a31a779af14867
MTeixeira24 <up201607941@fe.up.pt>
2018-08-07 20:57:01 +0100

    Removed test files and cleaned code on LightJason side

--- LightJason/src/main/java/jasonveins/App.java
+++ LightJason/src/main/java/jasonveins/App.java
@@ -1,9 +1,10 @@
 package jasonveins;
 
-import javax.annotation.Nonnull;
-import java.util.Set;
+import jasonveins.omnet.AgentManager;
+import jasonveins.omnet.ConnectionManager;
+
+
 import java.util.logging.LogManager;
-import java.util.stream.IntStream;
 
 
 /**
@@ -33,57 +34,10 @@ final class App
      */
     public static void main( final String[] p_args )
     {
-        if ( p_args.length < 2 )
-            throw new RuntimeException( "arguments are not set: ASL script, number of agents" );
-
-        // parameter of the command-line arguments:
-        // 1. ASL file
-        // 2. number of agents
-        // 3. number of iterations (if not set maximum integer)
-       /* final Set<MyAgent> l_agents;
-        try
-            (
-                final FileInputStream l_stream = new FileInputStream( p_args[0] );
-            )
-        {
-            // agent factory with generating an unmodifiable set
-            l_agents = Collections.unmodifiableSet(
-                new MyAgentGenerator( l_stream )
-                    .generatemultiple( Integer.parseInt( p_args[1] ) )
-                    .collect( Collectors.toSet() )
-            );
-        }
-        catch ( final Exception l_exception )
-        {
-            l_exception.printStackTrace();
-            return;
-        }
-
-        // runtime call (with parallel execution)
-        execute(l_agents, p_args);*/
-
-    }
-
-    final static void execute(@Nonnull final Set<MyAgent> l_agents, final String[] p_args){
-        IntStream
-                .range(
-                        0,
-                        p_args.length < 3
-                                ? Integer.MAX_VALUE
-                                : Integer.parseInt( p_args[2] )
-                )
-                .forEach( j -> l_agents.parallelStream()
-                        .forEach( i ->
-                        {
-                            try
-                            {
-                                i.call();
-                            }
-                            catch ( final Exception l_exception )
-                            {
-                                l_exception.printStackTrace();
-                                throw new RuntimeException();
-                            }
-                        } ) );
+        ConnectionManager cm = new ConnectionManager();
+        AgentManager am = new AgentManager("manager.asl", cm);
+        cm.setAgentManager(am);
+        cm.start();
+        am.loop();
     }
 }
