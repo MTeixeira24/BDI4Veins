56d76bb0da018c60b6c1d781e8f18d1b12b059da
MTeixeira24 <miguelangelo_teixeira@hotmail.com>
2018-10-12 15:15:38 +0100

    Started on revised traffic manager for join

---
+++ veinsBDI/src/traffic/NoisePlatoonTrafficManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * NoisePlatoonTrafficManager.cpp
+ *
+ *  Created on: Oct 12, 2018
+ *      Author: miguel
+ */
+
+#include "NoisePlatoonTrafficManager.h"
+
+NoisePlatoonTrafficManager::NoisePlatoonTrafficManager() {
+    // TODO Auto-generated constructor stub
+
+}
+
+NoisePlatoonTrafficManager::~NoisePlatoonTrafficManager() {
+    // TODO Auto-generated destructor stub
+}
+
---
+++ veinsBDI/src/traffic/NoisePlatoonTrafficManager.h
@@ -0,0 +1,17 @@
+/*
+ * NoisePlatoonTrafficManager.h
+ *
+ *  Created on: Oct 12, 2018
+ *      Author: miguel
+ */
+
+#ifndef TRAFFIC_NOISEPLATOONTRAFFICMANAGER_H_
+#define TRAFFIC_NOISEPLATOONTRAFFICMANAGER_H_
+
+class NoisePlatoonTrafficManager {
+public:
+    NoisePlatoonTrafficManager();
+    virtual ~NoisePlatoonTrafficManager();
+};
+
+#endif /* TRAFFIC_NOISEPLATOONTRAFFICMANAGER_H_ */
